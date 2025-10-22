e99156b5af67b7792e0c0e847ac9b3aaab73fc4d
SHaruto0 <szkharuto0407@gmail.com>
2025-10-22 00:48:58 -0400

    DQN

---
+++ RL/model.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DQN(nn.Module):
+    def __init__(self, input_dim, fc1_dim, fc2_dim, fc3_dim, n_action):
+        super().__init__()
+
+        self.fc1 = nn.Linear(input_dim, fc1_dim, bias=True)
+        self.fc2 = nn.Linear(fc1_dim, fc2_dim, bias=True)
+        self.fc3 = nn.Linear(fc2_dim, fc3_dim, bias=True)
+        self.output = nn.Linear(fc3_dim, n_action, bias=True)
+
+        self.device = "gpu" if torch.cuda.is_available() else "cpu"
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.output(x)
+        return x
+    
+if __name__ == "__main__":
+    model = DQN(1,2,3,4,5)
