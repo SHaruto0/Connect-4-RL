640808224eee36cd134301369f510a8a7172590e
SHaruto0 <szkharuto0407@gmail.com>
2026-01-03 20:06:26 -0800

    Clean unused code

--- RL/model.py
+++ RL/model.py
@@ -8,8 +8,12 @@ class DQN(nn.Module):
 
         self.fc1 = nn.Linear(input_dim, fc1_dim, bias=True)
         self.fc2 = nn.Linear(fc1_dim, fc2_dim, bias=True)
-        # self.fc3 = nn.Linear(fc2_dim, fc3_dim, bias=True)
         self.output = nn.Linear(fc2_dim, n_action, bias=True)
+
+        # Xavier weights initialization
+        nn.init.xavier_uniform_(self.fc1.weight)
+        nn.init.xavier_uniform_(self.fc2.weight)
+        nn.init.xavier_uniform_(self.output.weight)
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.to(self.device)
@@ -17,9 +21,9 @@ class DQN(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        # x = F.relu(self.fc3(x))
         x = self.output(x)
         return x
     
 if __name__ == "__main__":
-    model = DQN(1,2,3,4,5)
+    model = DQN(1,2,3,5)
+    print(model)
