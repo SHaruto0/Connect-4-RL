aaef1a5b08dc805561297b883a6a3d73d0ccd89c
SHaruto0 <szkharuto0407@gmail.com>
2026-01-03 20:24:21 -0800

    changed file name

--- RL/evaluate.py
+++ RL/evaluate.py
@@ -4,7 +4,7 @@ from model import DQN
 from Connect4Env import Connect4
 
 if __name__ == "__main__":
-    model_path = "models/stage5_model.pth"
+    model_path = "models/1_stage10_model.pth"
     agent = DQN(input_dim=2*6*7, fc1_dim=256, fc2_dim=256, n_action=7)
     agent.load_state_dict(torch.load(model_path))
 
