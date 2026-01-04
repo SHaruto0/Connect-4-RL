4789826c549c5241841c3cce6d2c6602711cb447
SHaruto0 <szkharuto0407@gmail.com>
2026-01-03 19:59:22 -0800

    Update default layer nodes count, batch size, memory size, and learning rate

--- RL/agent.py
+++ RL/agent.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 from model import DQN
 
 class Agent:
-    def __init__(self, input_dim, n_action, lr=0.01, batch_size=100, eps=1.0, min_eps=0.01, gamma=0.99, 
-                 fc1_dim=100, fc2_dim=100, fc3_dim=100, mem_size=10000, eps_dec=0.00005):
+    def __init__(self, input_dim, n_action, lr=0.001, batch_size=64, eps=1.0, min_eps=0.01, gamma=0.99, 
+                 fc1_dim=128, fc2_dim=128, fc3_dim=100, mem_size=50000, eps_dec=0.00001 ):
         self.lr = lr
         self.batch_size = batch_size
         self.eps = eps
