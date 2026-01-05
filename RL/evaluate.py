3c7ee0d0288c277ed4a0eed86f2f4cb3bf31879c
SHaruto0 <szkharuto0407@gmail.com>
2026-01-04 18:29:53 -0800

    Fix evaluate call

--- RL/evaluate.py
+++ RL/evaluate.py
@@ -4,8 +4,8 @@ from model import DQN
 from Connect4Env import Connect4
 
 if __name__ == "__main__":
-    model_path = "models/1_stage10_model.pth"
-    agent = DQN(input_dim=2*6*7, fc1_dim=256, fc2_dim=256, n_action=7)
+    model_path = "models/1/final_model.pth"
+    agent = DQN(input_dim=2*6*7, fc1_dim=100, fc2_dim=100, n_action=7)
     agent.load_state_dict(torch.load(model_path))
 
     env = Connect4(agent=agent)
